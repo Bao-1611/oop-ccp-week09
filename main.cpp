10decb926e6c151edef582c29418465631e5611c
Nguyen Chi Bao <ncbslav@gmail.com>
2026-09-23 10:37:55 +0700

    2nd feature

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 struct Food {
-    string id;        
-    string name;       
-    double price;      
+    string id;
+    string name;
+    double price;
     int quantity; 
 };
 
@@ -30,3 +30,39 @@ struct Restaurant {
     Order orders[100];
     int orderCount = 0;
 };
+
+// Chuc nang 1: Nhap thong tin cua hang
+void inputRestaurantInfo(Restaurant &res) {
+    cin.ignore();
+    cout << "\n=== NHAP THONG TIN CUA HANG ===\n";
+    cout << "Nhap ten cua hang: ";
+    getline(cin, res.name);
+    cout << "Nhap dia chi cua hang: ";
+    getline(cin, res.address);
+    cout << "Nhap so dien thoai: ";
+    getline(cin, res.phone);
+    cout << "=> Cap nhat thong tin cua hang thanh cong!\n";
+}
+
+// Chuc nang 2: Them mon an moi
+void addFood(Restaurant &res) {
+    if (res.foodCount >= 100) {
+        cout << "Danh sach mon an da day!\n";
+        return;
+    }
+    Food f;
+    cout << "\n=== THEM MON AN MOI ===\n";
+    cout << "Nhap ma mon: ";
+    cin >> f.id;
+    cin.ignore();
+    cout << "Nhap ten mon: ";
+    getline(cin, f.name);
+    cout << "Nhap don gia: ";
+    cin >> f.price;
+    cout << "Nhap so luong con lai: ";
+    cin >> f.quantity;
+
+    res.foods[res.foodCount] = f;
+    res.foodCount++;
+    cout << "=> Them mon an thanh cong!\n";
+}
