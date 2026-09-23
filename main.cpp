5f29c4cceac15d152e0e0b1e015f8578cef4b733
Nguyen Chi Bao <ncbslav@gmail.com>
2026-09-23 10:18:35 +0700

    practice for week 9

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Food {
+    string id;        
+    string name;       
+    double price;      
+    int quantity; 
+};
+
+struct Order {
+    string id;
+    string customerName;
+    string address;
+    Food food;
+    int quantity;
+    string status;
+};
+
+struct Restaurant {
+    string name;
+    string address;
+    string phone;
+    
+    Food foods[100];
+    int foodCount = 0;
+    
+    Order orders[100];
+    int orderCount = 0;
+};
